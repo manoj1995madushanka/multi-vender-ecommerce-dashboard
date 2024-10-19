9e8f4f28a173e4c3d364b4a27d95e5553c0b8088
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-10-19 22:37:15 +0530

    fix route error and add initialize routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
 import {useState} from "react";
 import Router from "./router/Router";
+import publicRoutes from "./router/routes/publicRoutes";
 
 function App() {
-  const [allRoutes, setAllRoutes] = useState([])
+  const [allRoutes, setAllRoutes] = useState([...publicRoutes])
   return <Router allRoutes={allRoutes}/>
 }
 
