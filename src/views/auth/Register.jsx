30e3b7b041d6e438e56e5498a05195ea73862f86
manoj1995madushanka <madushankamanoj414@gmail.com>
2024-10-20 21:21:33 +0530

    register page social media icon section

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import {FaFacebook, FaGoogle} from "react-icons/fa";
 
 const Register = () => {
   return (
@@ -43,6 +44,29 @@ const Register = () => {
             <div className="flex items-center mb-3 gap-3 justify-center">
               <p>Already have an account <Link className="font-bold" to="/login">Sign In</Link></p>
             </div>
+
+            <div className="w-full flex justify-center items-center mb-3">
+              <div className="w-[45%] bg-slate-700 h-[1px]"></div>
+              <div className="w-[10%] flex justify-center items-center">
+                <span className="pb-1">Or</span>
+              </div>
+              <div className="w-[45%] bg-slate-700 h-[1px]"></div>
+            </div>
+
+            <div className="flex justify-center items-center gap-3">
+
+              <div
+                className="w-[135px] h-[35px] flex rounded-md bg-orange-700 shadow-lg hover:shadow-orange-700/50 justify-center cursor-pointer items-center overflow-hidden">
+                <span><FaGoogle/></span>
+              </div>
+
+              <div
+                className="w-[135px] h-[35px] flex rounded-md bg-blue-700 shadow-lg hover:shadow-blue-700/50 justify-center cursor-pointer items-center overflow-hidden">
+                <span><FaFacebook/></span>
+              </div>
+
+            </div>
+
           </form>
         </div>
       </div>
